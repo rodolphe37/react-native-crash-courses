d0ea943719a943207ec67942df1b6f123601d210
rodolphe37 <rodolphe.a@gmail.com>
2019-11-16 21:23:41 +0100

    flexBox exercice

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import { StyleSheet, TextInput, View, Button } from 'react-native';
 export default function App() {
   
   return (
-    <View style={{padding: 50}}>
-      <TextInput placeholder="Ecrivez ici" style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
-      <Button title="Ajouter"/>
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput placeholder="Ecrivez ici" 
+        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}/>
+        <Button title="Ajouter"/>
+      </View>
     </View>
   );
 }
