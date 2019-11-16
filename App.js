d29c7a121dfee16f5d86a38e9b0524a5d0128a69
rodolphe37 <rodolphe.a@gmail.com>
2019-11-16 21:16:40 +0100

    ajout d'un TextInput ansi qu'un button --simple

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <TextInput placeholder="Ecrivez ici" style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
+      <Button title="Ajouter"/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
