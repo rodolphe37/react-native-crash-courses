34c6c72ef7362779d6bf2a8d4283c16b9d612e26
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 09:29:23 +0100

    exercice2 flexbox

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 
 export default function App() {
   
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput placeholder="Ecrivez ici" 
-        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}/>
-        <Button title="Ajouter"/>
-      </View>
+    <View 
+      style={{ 
+        padding: 50, 
+        flexDirection: 'row', 
+        width: '80%', 
+        height: 300, 
+        justifyContent: 'space-around', 
+        alignItems: 'stretch' 
+      }}
+    >
+    <View 
+      style={{ 
+        backgroundColor: 'blue',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>1</Text>
     </View>
+    <View 
+      style={{ 
+        backgroundColor: 'red',
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>2</Text>
+    </View>
+    <View 
+      style={{ 
+        backgroundColor: 'green',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>3</Text>
+    </View>
+  </View>
   );
 }
