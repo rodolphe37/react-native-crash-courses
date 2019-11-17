8eee093be079751db5c194796aa56d3218d6315b
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 20:48:34 +0100

    Add alert window when the number is invalid

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import {
     Text,
     Button,
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -30,7 +31,12 @@ const StartGameScreen = props => {
         const confirmInputHandler = () => {
               const choseNumber = parseInt(enteredValue);
               if (choseNumber === NaN || choseNumber <= 0 || choseNumber > 99) {
-                  return;
+                Alert.alert(
+                    'Invalid Number',
+                    'Number has to be a number between 1 and 99.',
+                    [{text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+                ); 
+                return;
               }
             setConfirmed(true);
             setSelecteNumber(choseNumber);
