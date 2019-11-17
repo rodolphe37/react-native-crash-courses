ba205ba2359594133c73cc774e7d4e01664dde20
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 18:05:10 +0100

    Add Game Screen Module

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
+      <StartGameScreen />
     </View>
   );
 }
