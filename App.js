f8d434660c20df7061804887910927131d751387
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 09:49:38 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-<Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text ou Output text!')} />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput
+          placeholder="Ecrivez Ici !"
+          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+        />
+        <Button title="Ajouter" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
+
 });
