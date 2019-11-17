112e09ef90d9d399dc4b1f495f8be107b5d8e72f
rodolphe37 <rodolphe.a@gmail.com>
2019-11-18 00:08:39 +0100

    Add GameOver function with useEffect Hooks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -19,8 +19,17 @@ const GameScreen = props => {
      const [currentGuess, setCurrentGuess] = useState(
          generateRandomBetween(1, 100, props.userChoice)
         );
+        const [rounds, setRounds] = useState(0);
         const currentLow = useRef(1);
-        const currentHigh = useRef(100)
+        const currentHigh = useRef(100);
+
+        const {userChoice, onGameOver} = props;
+
+        useEffect(() => {
+            if (currentGuess === userChoice) {
+                onGameOver(rounds);
+            }
+        }, [currentGuess, ]);
 
         const nextGuessHandler = direction => {
           if (
@@ -38,7 +47,8 @@ const GameScreen = props => {
               currentLow.current = currentGuess;
           }
           const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
-          setCurrentGuess(nextNumber)
+          setCurrentGuess(nextNumber);
+          setRounds(curRounds => curRounds + 1);
         };
 
         return (
