5a9400b593f1871a38951936606464e8c9a45b4f
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 10:34:07 +0100

    exercice sur les inputs, les evenements onPress, les lists et la scrollView

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Text, View, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const textInputHandler = (enteredText) => {
+      setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+    <ScrollView>
+    <View style={styles.screen}>
+      <View style={styles.viewScreen}>
         <TextInput
           placeholder="Ecrivez Ici !"
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+          style={styles.input}
+          onChangeText={textInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Ajouter" />
+        <Button title="Ajouter" onPress={addGoalHandler}  />
+      </View>
+      <View>
+        <ScrollView>
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItem} ><Text>{goal}</Text></View>)}
+        </ScrollView>
       </View>
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  viewScreen: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#CCC',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
 
 });
