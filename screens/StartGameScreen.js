a10758106828f0f07645439ea7953bb8c7d63da0
rodolphe37 <rodolphe.a@gmail.com>
2019-11-17 20:06:22 +0100

    personalization of input field and keyboard number only

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import { View, StyleSheet, Text, Button} from 'react-native';
 import Card from '../components/Card';
+import Input from '../components/Input';
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
@@ -9,7 +10,13 @@ const StartGameScreen = props => {
         <Text style={styles.title}>Start a New Game!</Text>
        <Card style={styles.inputContainer}>
             <Text>Select a Number</Text>
-            <TextInput />
+            <Input style={styles.Input} 
+              blurOnSubmit
+              autoCapitalize= 'none'
+              autoCorrect={false}
+              keyboardType="number-pad"
+              maxLength={2}
+            />
             <View style={styles.buttonContainer}>
                 <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
                 <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
@@ -46,6 +53,10 @@ const styles = StyleSheet.create({
     button: {
         width: 90,
 
+    },
+    input: {
+        width: 70,
+        textAlign: 'center'
     }
 });
 
